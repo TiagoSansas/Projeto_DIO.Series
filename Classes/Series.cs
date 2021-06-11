0610c226c44cd161df9cbc9b7f209666080c70e3
Tiago Souza <tiagoeduardosouza@gmail.com>
2021-06-11 19:18:17 -0300

    Criando Constructor de Series, apresentação dos  atributos, e metodos de retornar titulo e id.

--- Classes/Series.cs
+++ Classes/Series.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DIO.Series
 {
     public class Series : EntidadeBase
@@ -8,8 +10,33 @@ namespace DIO.Series
         private string Descricao { get; set; }
         private int Ano { get; set; }
 
-        
+        //Constructor
+        public Series(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
 
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno +="Genero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Inicio" + this.Ano;
+            return retorno;
+        }
 
+        // Metodos 
+        public string retornaTitulo(){
+            return this.Titulo;
+        }
+        public int retornaId(){
+            return this.Id;
+        }
     }
+
 }
