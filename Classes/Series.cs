d214a94e02fe1ce965ea5d952ec0a79d7db99c77
Tiago Souza <tiagoeduardosouza@gmail.com>
2021-06-11 19:07:13 -0300

    Criação das Classes EntidadeBase e Series e o Enum de Genero

---
+++ Classes/Series.cs
@@ -0,0 +1,15 @@
+namespace DIO.Series
+{
+    public class Series : EntidadeBase
+    {
+        // Atributos
+        private Genero Genero {get; set;}
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        
+
+
+    }
+}
